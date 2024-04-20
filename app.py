f9fc11312675a9b156156b1b24b6805dc2a79cf5
anthonycs4 <165516654+anthonycs4@users.noreply.github.com>
2024-04-19 19:48:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import plotly.express as px
 from shiny.express import input, render, ui
 from shinywidgets import render_plotly
 import seaborn as sns
+import ipyleaflet as ipyl
 
 # Cargar el conjunto de datos Titanic
 titanic_df = sns.load_dataset('titanic')
-
-# Establecer las opciones de la página
-#ui.page_opts(title="Multi-column example")
 
 # Menú desplegable para seleccionar una variable del Titanic
 ui.input_select("var", "Select variable", choices=titanic_df.columns.tolist())
@@ -26,9 +24,7 @@ with ui.layout_columns(height="300px"):
     def table():
         return titanic_df
 
-# Segundo conjunto de UI
-
-# Menú desplegable para seleccionar una variable (segunda parte)
+# Menú desplegable para seleccionar variables adicionales
 with ui.layout_columns():
     ui.input_select("var1", None, choices=titanic_df.columns.tolist(), width="100%")
     ui.input_select("var2", None, choices=titanic_df.columns.tolist(), width="100%")
@@ -45,23 +41,6 @@ with ui.layout_columns():
         p = px.histogram(titanic_df, x=input.var2())
         p.update_layout(height=200, xaxis_title=None)
         return p
-
-
-from shiny import reactive
-from shiny.express import input, ui
-from shinywidgets import render_widget
-import ipyleaflet as ipyl
-import seaborn as sns
-
-# Cargar el conjunto de datos Titanic
-titanic_df = sns.load_dataset('titanic')
-
-# Obtener una lista de nombres de ciudades basada en las ciudades de embarque en el Titanic
-city_centers = {
-    "Cherbourg": (49.6500, -1.6500),
-    "Londres": (51.5074, -0.1278),
-    "Southampton": (50.9000, -1.4000)
-}
 
 # Menú desplegable para seleccionar un centro de mapa
 ui.input_select("center", "Center", choices=list(city_centers.keys()))
